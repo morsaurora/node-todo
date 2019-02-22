b174de000621595e58477acf27a42c775b2764ad
BinarYXSynthesis <azurris@gmail.com>
2019-02-22 21:46:04 +0530

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)) {
+        return res.status(400).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(todo) {
+            return res.send({todo});
+        }
+        res.status(404).send();
+    }, (e) => {
+        res.status(400).send();
+    })
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
