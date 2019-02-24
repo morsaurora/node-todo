2e5ba3b1d6be5e088cc305f7f00a8c01b461c352
BinarYXSynthesis <azurris@gmail.com>
2019-02-24 18:33:15 +0530

    refactored db connection strings for heroku and localhost

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,17 @@
 var mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://mors:Milan_99_Italy@ds349045.mlab.com:49045/mongo-todo' || 'mongodb://localhost:27017/TodoApp');
+
+var conn_str = undefined;
+
+if(process.env.prod) {
+    console.log('connected to heroku');
+    conn_str = 'mongodb://mors:Milan_99_Italy@ds349045.mlab.com:49045/mongo-todo';
+}
+else {
+    console.log('connected to localhost');
+    conn_str = 'mongodb://localhost:27017/TodoApp';
+}
+mongoose.connect(conn_str);
 
 module.exports = {mongoose};
