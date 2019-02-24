5caab613fb67385b56be9fbac10bc8ca5269f24d
BinarYXSynthesis <azurris@gmail.com>
2019-02-24 18:44:10 +0530

    added db connection strings for heroku and localhost environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,11 +5,9 @@ mongoose.Promise = global.Promise;
 var conn_str = undefined;
 
 if(process.env.prod) {
-    console.log('connected to heroku');
     conn_str = 'mongodb://mors:Milan_99_Italy@ds349045.mlab.com:49045/mongo-todo';
 }
 else {
-    console.log('connected to localhost');
     conn_str = 'mongodb://localhost:27017/TodoApp';
 }
 mongoose.connect(conn_str);
